0d8faea9181c78690ba802fd8d9f456060992ad2
Ben <benroberts96@hotmail.co.uk>
2020-08-22 16:59:50 +0100

    Fix logging

--- renewal_reminder/app.py
+++ renewal_reminder/app.py
@@ -1,13 +1,11 @@
+import logging
 from dataclasses import dataclass
-from logging import getLogger
 from os import environ
 
 from renewal_reminder.business_logic.checker import Checker
 from renewal_reminder.infrastructure.messenger.bot import BotMessenger
 from renewal_reminder.infrastructure.renewals import Renewals
 from renewal_reminder.infrastructure.storage.csv import CsvMembersRetriever
-
-LOGGER = getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -32,7 +30,7 @@ class AppConfig:
 def main():
     config = AppConfig.read_from_environment()
 
-    LOGGER.setLevel(config.log_level)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=config.log_level)
 
     messenger = BotMessenger(token_id=config.token_id, chat_id=config.chat_id)
     renewals = Renewals(notice_days=config.notice_days)
